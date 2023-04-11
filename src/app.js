bab9e107a1e08171fa863ba3f1e88c416ddb8043
Joacoder <joacoder.dev@gmail.com>
2023-04-11 12:48:08 -0300

    LoggerActualizado

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ import cookieParser from "cookie-parser";
 import __dirname from "./utils.js";
 import config from "./config/config.js";
 import apiCart from "./routes/cart.router.js";
-import { logger } from "./middlewares/logger.js";
+import { logger } from "./logger.js"
+import loggerRoutes from "./routes/loggerRoutes.js"
 import apiProd from "./routes/product.router.js";
 import viewsRouter from "./routes/views.rotuer.js";
 import {userRouter} from "./routes/user.routes.js";
@@ -25,20 +26,51 @@ initializeStrategy();
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use(cookieParser());
+app.get('/', (req, res) =>{
+    // console.log("Ruta solicitada")
+    req.logger.info('Ruta Solicitada')
+    res.send({message:"Prueba de Logger"})
+})
+app.get("/login",(req,res)=>{
+    req.logger.warn("Advertencia, el usuario no existe")
+    res.send("Login autorizado");
+})
 app.use("/", viewsRouter);
 app.use("/api/productos", apiProd);
+app.get('/operacionsencilla', (req, res) => {
+    let sum = 0;
+    for (let i = 0; i < 1000000; i++) {
+        sum += i
+    } 
+    res.send({ sum });
+})
+app.get('/operacioncompleja', (req, res) => {
+    let sum = 0;
+    for (let i = 0; i < 5e8; i++) {
+        sum += i
+    } 
+    res.send({ sum });
+})
 app.use("/api/carrito", apiCart);
 app.use("/api/sessions/", sessionsRouter);
 app.use("/api/users", userRouter);
 app.use("/api/mockingproducts", mockingrouter)
-app.listen(PORT, () => { console.log(`Corriendo el servidor en el puerto${PORT}`) });
+app.listen(PORT, () => logger.info(colors.green(`Corriendo el servidor exitosamente en el puerto... ${PORT}`)))
 const environment = () => {
     mongoose.set('strictQuery', false);
     mongoose.connect(DB_URL, (err) => {
         if (err) {
-            console.log('No se puede conectar a la base de datos')
+            logger.error(colors.red("Error...No se puede conectar a la base de datos..."));
         } else {
-            console.log('Conectado a la base de datos')
+            logger.info(colors.green('Conectado exitosamente a la base de datos.'))
         }
     })
 }
+
+app.use((req, res, next) => {
+    logger.debug(`${req.method} ${req.url}`);
+    next();
+  });
+
+  app.use("/api/loggerTest", loggerRoutes);
+
